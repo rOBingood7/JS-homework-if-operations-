a4972fac3aa57db3687353383b844f17b4be69b1
rOBingood7 <mirkomilmuhtorov22@gmail.com>
2024-03-30 15:26:53 +0500

    homework

---
+++ index.js
@@ -0,0 +1,31 @@
+/* let guestlist = "Putin Biden Zelenskiy Lukashenko Mirziyoyev Makaron Erdogan"
+
+let name = prompt('What is your name, mr president?').toLowerCase().trim()
+
+if (name === "putin" || name === "biden" || name === "zelenskiy" || name === "lukashenko" || name === "mirziyoyev" || name === "makaron" || name === "erdogan") {
+    alert('Welcome');
+} else {
+    alert('Bye');
+} */
+
+
+
+/* let num = prompt('any num');
+
+
+if (num % 2 === 0) {
+    alert('Чётное');
+} else {
+    alert('Нечётное');
+}
+ */
+
+
+/* let name = prompt('What is your name?').toLowerCase().trim()
+
+if (name.charAt(0).toLowerCase() === 'a') {
+    alert('Welcome');
+} else {
+    alert('Bye');
+} */
+
